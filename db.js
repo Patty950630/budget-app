27e48092b5a0d64fc75a2f99301726dda714b200
Pattylee <peichenl886@gmail.com>
2025-05-26 20:11:35 +0800

    調整路徑使用 __dirname，讓 render 找到資料庫

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join('C:/sqlite_project/budget.sqlite');
+const dbPath = path.join(__dirname, 'budget.sqlite');
+
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {

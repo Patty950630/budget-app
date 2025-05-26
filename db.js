e16e6be0771d08a4a4c0448d59323ff30c126ec1
Pattylee <peichenl886@gmail.com>
2025-05-26 20:20:00 +0800

    修正資料庫為正確版本（含 records 表格）

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
+// ✅ 使用相對路徑指向專案內的 budget.sqlite
 const dbPath = path.join(__dirname, 'budget.sqlite');
-
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
